cff2ff24d5ae1a2fb794d892e49ed03883156e2a
Bears0haunt <bearshaunt0@gamil.com>
2024-02-27 19:34:45 +0800

    changed to my work env
    1. update gtest third-party
    2. set debug env

--- sigmod-2018/compile.sh
+++ sigmod-2018/compile.sh
@@ -5,5 +5,5 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
 mkdir -p build/release
 cd build/release
-cmake -DCMAKE_BUILD_TYPE=Release -DFORCE_TESTS=OFF ../..
+cmake -DCMAKE_BUILD_TYPE=Debug -DFORCE_TESTS=OFF ../..
 make -j8
